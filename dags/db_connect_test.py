fa59b33264073c76b09eb876fc4f358a268f082a
syjeon <sum9611@khu.ac.kr>
2023-04-13 15:33:00 +0900

    크롤링 코드 추가

--- dags/db_connect_test.py
+++ dags/db_connect_test.py
@@ -1,22 +1,22 @@
-import datetime
+# import datetime
 
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.decorators import dag
-import pymysql
+# from airflow import DAG
+# from airflow.operators.python import PythonOperator
+# from airflow.decorators import dag
+# import pymysql
 
 
-conn = pymysql.connect(host='127.0.0.1', user = 'root', password = 'root', db = 'mysql', charset='utf8', port = 3307)
+# conn = pymysql.connect(host='127.0.0.1', user = 'root', password = 'root', db = 'mysql', charset='utf8', port = 3307)
 
-cur = conn.cursor()
-with DAG(
-    dag_id="db_connect_test",
-    start_date=datetime.datetime(2023, 4, 3),
-    schedule="@once",
-) as dag:
+# cur = conn.cursor()
+# with DAG(
+#     dag_id="db_connect_test",
+#     start_date=datetime.datetime(2023, 4, 3),
+#     schedule="@once",
+# ) as dag:
 
-    @dag.task(task_id="mariadb connect")
-    def print_context():
-        sql = 'show talbes'
-        cur.execute(sql)
-        conn.close()
+#     @dag.task(task_id="mariadb connect")
+#     def print_context():
+#         sql = 'show talbes'
+#         cur.execute(sql)
+#         conn.close()
